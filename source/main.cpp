df34ec7d86191c872fe474bb78337f47e20fda3c
Johan Wagenheim <johan@dospuntos.no>
2024-04-01 22:25:27 -0400

    Improve window alignment for translations and different font sizes

--- source/main.cpp
+++ source/main.cpp
@@ -15,6 +15,12 @@ Resizer::Resizer() : BApplication("application/x-vnd.TAResizer")
 	Fenetre = new MainWindow(); //fenetre ou est l'image
 	Option = new OptionWindow(); //fenetre d'option
 	Mouse = new MouseWindow(); //fenetre de la souris
+
+	Option->UpdateSizeLimits();
+	int32 x = Option->Frame().RightTop().x + 10; //for some reason the x coordinate is 10px off
+	int32 y = Option->Frame().RightTop().y;
+	Fenetre->MoveTo(x, y);
+
 	Fenetre->Show();
 	Option->Show();
 	Mouse->Run();
